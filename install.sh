53289f74a9bd0cefd4dd7dc2348df57e0822e17f
Oleg <32685630+Qadosch@users.noreply.github.com>
2025-09-16 15:45:51 +0200

    fix

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ sudo apt install htop -y
 sudo apt install zsh -y
 KEEP_ZSHRC=true RUNZSH=false sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone git://github.com/zsh-users/zaw.git ~/.oh-my-zsh/custom/plugins/zaw
+git clone https://github.com/zsh-users/zaw.git ~/.oh-my-zsh/custom/plugins/zaw
 
 bash -c "$(curl --silent https://raw.githubusercontent.com/thoughtworks/talisman/main/global_install_scripts/install.bash)"
